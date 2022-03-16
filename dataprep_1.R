545cea8b23afcab4a85d56e43735fce5aa795469
Cam Appel <cameron@ourworldindata.org>
2022-03-16 13:58:21 -0400

    enhance(working)

--- dataprep_1.R
+++ dataprep_1.R
@@ -12,12 +12,12 @@ COUNTRIES <- sort(unique(raw_df$country))
 
 # lod_1 <- list()
 # for(COUNTRY in COUNTRIES){
-list_1 <- lapply(COUNTRIES, function(COUNTRY){ #{COUNTRY="US"}
+list_1 <- lapply(COUNTRIES, function(COUNTRY){ #{COUNTRY="Australia"}
     cat(paste0("\nProcessing data for", " ", COUNTRY))
     dir.create(COUNTRY)
     file.copy("raw_df.csv", paste0(COUNTRY, "/", "raw_df.csv"), overwrite=T)
     country_df <- raw_df %>% 
-        filter(country %in% c(COUNTRY)) #%>% filter(!is.na(description), description!="")
+        filter(country %in% c(COUNTRY)) %>% filter(!is.na(description), description!="")
     DIMENSIONS <- sort(unique(country_df$dimension))
     # lapply(DIMENSIONS, function(DIMENSION){
     #     country_df %>% filter(dimension %in% DIMENSION)
@@ -25,54 +25,65 @@ list_1 <- lapply(COUNTRIES, function(COUNTRY){ #{COUNTRY="US"}
     country_dimension_lod <- country_df %>% split(., .[["dimension"]]) %>%
         lapply(., function(df){ #{df=country_df %>% split(., .[["dimension"]]) %>% .[[1]]; DIMENSION="Earnings Dispersion"}
             if(any(df$dimension=="Earnings Dispersion")){
-                df %<>% 
-                    filter(!is.na(description), description!="") %>% #filter(measure=="Top decile as % median") %>%
-                    select(one_of("year", "value"))
+                # country_df %>% select(one_of("dimension", "welfare_concept")) %>% unite(., one_of("dimension", "welfare_concept"), sep=" - ", na.rm=T,remove=T)
+                
+                newvaluecolname <- df %>% unite(., "newcol", one_of("dimension", "welfare_concept"), sep=" - ", na.rm=T, remove = T) %>% select(newcol) %>% distinct() %>% .[[1]] %>% .[[1]]
+                df <- df %>%
+                    select(one_of("year", "value")) %>%
+                    setNames(recode(names(.), "value"=newvaluecolname))
                 readr::write_csv(df, paste0(COUNTRY, "/top_chart.csv"))
             }
             df
         })
-    # LH_lod <- country_dimension_lod[names(country_dimension_lod)!="Earnings Dispersion"] %>%
-    #     lapply(., function(d){ #{d=country_dimension_lod$Poverty}
-    #         d %>% select(matches("gini|legend")) %>% distinct()
-    #     })
-    country_df %>% 
-        filter(legend %in% c("Gini coefficient - Equivalised disposable household income", "Gini coefficient - Income (tax units)", "Share below 60% median", "Wealth Inequality", "Top Income Shares"))
+    LH_lod <- country_dimension_lod[names(country_dimension_lod)!="Earnings Dispersion"] %>%
+        lapply(., function(d){
+            d <- filter(d, preferred_definition=="*")
+            tryCatch({
+                newvaluecolname <- d %>% unite(., "newcol", one_of("dimension", "welfare_concept"), sep=" - ", na.rm=T, remove = T) %>% select(newcol) %>% distinct() %>% .[[1]] %>% .[[1]]
+            d %>% select(one_of("year", "value")) %>%
+                setNames(recode(names(.), "value"=newvaluecolname))},
+            error=function(e){d})
+        })
+        df <- plyr::join_all(LH_lod)
+        readr::write_csv(df, paste0(COUNTRY, "/", "bottom_chart.csv"))
+        
+    # country_df %>% 
+    #     filter(legend %in% c("Gini coefficient - Equivalised disposable household income", "Gini coefficient - Income (tax units)", "Share below 60% median", "Wealth Inequality", "Top Income Shares"))
     
     # {readr::read_csv("/Users/rhoadss/srhoads/cam/chartbook/UK/series/UK_LH.csv") %>% slice(-(1:110)) %>% datapasta::df_paste()}
-    if(TEMP<-T){
-        LH_temp_df <- tibble(
-        year = c(2010,2011,
-                 2012,2013,2014,
-                 2015,2016,2017,
-                 2018,2019,2020,2021),
-        `Gini coefficient - Equivalised disposable household income` = c(33.71587,
-                                                                         34.00962,33.63875,
-                                                                         34.25986,33.94323,
-                                                                         34.68564,33.65,
-                                                                         34.20763,35.04541,
-                                                                         35.17274,NA,NA),
-        `Gini coefficient - Income (tax units)` = c(NA,NA,NA,
-                                                    NA,NA,NA,NA,NA,
-                                                    NA,NA,NA,NA),
-        `Share below 60% median` = c(15.92449047,15.75767364,
-                                     15.43336331,15.2656601,
-                                     15.9083579,
-                                     16.31999248,16.22156218,
-                                     17.16024766,NA,NA,
-                                     NA,NA),
-        `Wealth Inequality` = c(NA,NA,
-                                19.88,NA,NA,NA,NA,
-                                NA,NA,NA,NA,NA),
-        `Top Income Shares` = c(12.15,
-                                13.02,12.63,13.69,
-                                13.23,12.25,12.41,
-                                13.3,12.96,12.6,
-                                12.65,12.65)
-    )
-        
-        readr::write_csv(LH_temp_df, paste0(COUNTRY, "/", COUNTRY, "_LH.csv"))
-        }
+    # if(TEMP<-T){
+    #     LH_temp_df <- tibble(
+    #     year = c(2010,2011,
+    #              2012,2013,2014,
+    #              2015,2016,2017,
+    #              2018,2019,2020,2021),
+    #     `Gini coefficient - Equivalised disposable household income` = c(33.71587,
+    #                                                                      34.00962,33.63875,
+    #                                                                      34.25986,33.94323,
+    #                                                                      34.68564,33.65,
+    #                                                                      34.20763,35.04541,
+    #                                                                      35.17274,NA,NA),
+    #     `Gini coefficient - Income (tax units)` = c(NA,NA,NA,
+    #                                                 NA,NA,NA,NA,NA,
+    #                                                 NA,NA,NA,NA),
+    #     `Share below 60% median` = c(15.92449047,15.75767364,
+    #                                  15.43336331,15.2656601,
+    #                                  15.9083579,
+    #                                  16.31999248,16.22156218,
+    #                                  17.16024766,NA,NA,
+    #                                  NA,NA),
+    #     `Wealth Inequality` = c(NA,NA,
+    #                             19.88,NA,NA,NA,NA,
+    #                             NA,NA,NA,NA,NA),
+    #     `Top Income Shares` = c(12.15,
+    #                             13.02,12.63,13.69,
+    #                             13.23,12.25,12.41,
+    #                             13.3,12.96,12.6,
+    #                             12.65,12.65)
+    # )
+    #     
+    #     readr::write_csv(LH_temp_df, paste0(COUNTRY, "/", COUNTRY, "_LH.csv"))
+    #     }
      if(modify_html<-T){
         mainfile <- "chartbook.html"
         newfile <- paste0(COUNTRY, "/", mainfile)
@@ -81,16 +92,16 @@ list_1 <- lapply(COUNTRIES, function(COUNTRY){ #{COUNTRY="US"}
         file.copy(format_files_to_copy, paste0(COUNTRY, "/", format_files_to_copy))
         newfilelines <- readLines(newfile)
         
-        PATTERN="UK|United Kingdom"
+        PATTERN="change_country"
         country_linenums <- newfilelines %>% grep(PATTERN, .)
         for(i in country_linenums){
             # linetoeditcon <- readLines(newfile, -1)
             newfilelines[i] <- gsub(PATTERN, COUNTRY, newfilelines[i])
             # writeLines(newfilelines,newfile)
         }
-        PATTERN="Earnings Dispersion"
+        PATTERN="Earnings Dispersion\\.csv"
         for(i in grep(PATTERN, newfilelines)){
-            newfilelines[i] <- gsub(PATTERN, "top_chart", newfilelines[i])
+            newfilelines[i] <- gsub(PATTERN, "top_chart.csv", newfilelines[i])
         }
         PATTERN="series\\/"
         for(i in grep(PATTERN, newfilelines)){
