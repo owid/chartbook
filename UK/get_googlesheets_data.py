21c2c96c28661082bb820be181335427885a984f
rhoadss <rhoadss@jacksonlewis.com>
2022-03-16 00:47:01 -0400

    Sam initial stuff to prep for project transition to R

---
+++ UK/get_googlesheets_data.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import pandas as pd
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1jLNfP3iuteUJrH0zS9qWONskyKh9pFcl1hKSlgEc-I8"
+SAMPLE_RANGE_NAME = "All Data!A:N"
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+
+        dfObj = pd.DataFrame(values)
+        dfObj.columns = dfObj.iloc[0]
+        dfObj = dfObj[1:]
+
+        dfObj.to_csv(
+            "raw_df.csv",
+            index=False,
+        )
+        print("Writing...\n", "raw_df.csv")
+
+    except HttpError as err:
+        print(err)
+        
+        
+if __name__ == "__main__":
+    main()
+
