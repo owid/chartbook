5eb80c3a803d572bfd1ac6d53b817787adaaba9c
Cam Appel <cameron@ourworldindata.org>
2022-06-16 20:24:43 +0100

    clean(ind)

--- India/final_series_chart.js
+++ India/final_series_chart.js
@@ -12,51 +12,42 @@ var topincomecolor = '#6a3d9a'; // purple
 // Edit names and colours of the final series
 var series = [
     {
-        key: "Gini coefficient - Per capita household consumption", // XXX
         values: [],
         color: overallinequalitycolor
 
     },
     {
-        key: "Gini coefficient - Equivalised disposable household income", // XXX
         values: [],
         color: overallinequalitycolor
 
     },
     {
-        key: "Share of top 1% - Pre-tax national income (equal-split adults)", // XXX
         values: [],
         color: topincomecolor
     },
 
     {
-        key: "Share of top 1% - Pre-tax fiscal income (equal-split adults) (excluding capital gains)", // XXX
         values: [],
         color: topincomecolor
     },
 
     {
-        key: "Share below absolute poverty line (Lakdawala methodology) - Per capita household consumption", // XXX
         values: [],
         color: povertycolor
     },
     {
-        key: "Share below absolute poverty line (Tendulkar methodology) - Per capita household consumption", // XXX
         values: [],
         color: povertycolor
     },
     {
-        key: "Share below absolute poverty line (Rangarajan methodology) - Per capita household consumption", // XXX
         values: [],
         color: povertycolor
     },
     {
-        key: "Gini coefficient - Daily individual earnings of regular workers", // XXX
         values: [],
         color: earningscolor
     },
     {
-        key: "Share of top 1% - Individual net wealth", // XXX
         values: [],
         color: wealthcolor
     },
@@ -70,7 +61,9 @@ c3.csv("bottom_chart.csv", function (error, csv) {
     console.log("there are " + csv.length + " elements in my csv set");
 
     // Creates array of column names 
-    var column_names = series.map(item => { return item.key });; // XXX
+    var column_names = [Object.keys(csv[0])][0]; // XXX
+    column_names.shift()
+
     console.log(column_names)
 
     // Modifies years to fit chart
@@ -583,7 +576,6 @@ c3.select('#chart2')
 
 
 
-
 /* ---------------------------------------------------- */
 // Credits
 /* ---------------------------------------------------- */
@@ -594,23 +586,23 @@ var xorigin = 8;
 var yorigin = 1;
 
 /*1. Zeile*/
-// creditsCanvas
-//     .append("text")
-//     .text("A. B. Atkinson, J. Hasell, S. Morelli and M. Roser (2017) – 'The Chartbook of Economic Inequality' at")
-//     .attr("x", xorigin + 0)
-//     .attr("y", yorigin + 10)
-//     .attr("id", 'erstesTextstuckZweiteZeile').attr('class', 'creditsText');
+creditsCanvas
+    .append("text")
+    .text("A. B. Atkinson, J. Hasell, S. Morelli, M. Roser and C. Appel (2022) – 'The Chartbook of Economic Inequality' at")
+    .attr("x", xorigin + 0)
+    .attr("y", yorigin + 10)
+    .attr("id", 'erstesTextstuckZweiteZeile').attr('class', 'creditsText');
 
-// var erstesTextstuckZweiteZeileLange = document.getElementById('erstesTextstuckZweiteZeile').getComputedTextLength();
+var erstesTextstuckZweiteZeileLange = document.getElementById('erstesTextstuckZweiteZeile').getComputedTextLength();
 
-// creditsCanvas.append("svg:a")
-//     .attr("xlink:href", "http://www.chartbookofeconomicinequality.com")
-//     .attr("target", "_blank")
-//     .append("svg:text")
-//     .attr("x", xorigin + erstesTextstuckZweiteZeileLange + 3)
-//     .attr("y", yorigin + 10)
-//     .attr('class', 'creditsLink')
-//     .text("www.ChartbookOfEconomicInequality.com");
+creditsCanvas.append("svg:a")
+    .attr("xlink:href", "http://www.chartbookofeconomicinequality.com")
+    .attr("target", "_blank")
+    .append("svg:text")
+    .attr("x", xorigin + erstesTextstuckZweiteZeileLange + 3)
+    .attr("y", yorigin + 10)
+    .attr('class', 'creditsLink')
+    .text("www.ChartbookOfEconomicInequality.com");
 
 
 /*2. Zeile left*/
@@ -628,9 +620,7 @@ creditsCanvas.append("text")
     .attr("x", xorigin + erstesTextstuckMeasureLinkLange + 3)
     .attr("y", yorigin + 26)
     .attr('class', 'creditsText').attr("id", 'zweitesTextstuckMeasureLink')
-    .text("for an explanation of the measures of economic inequality.");
-var zweitesTextstuckMeasureLinkLange = document.getElementById('zweitesTextstuckMeasureLink').getComputedTextLength();
-
+    .text("for an explanation of the measures of economic inequality");
 
 
 /*2. Zeile – right*/
@@ -658,15 +648,15 @@ creditsCanvas
     .text("View the")
     .attr("x", xorigin + 0)
     .attr("y", yorigin + 40)
-    .attr("id", 'erstesTextstuckZweiteZeile').attr('class', 'creditsText');
+    .attr("id", 'erstesTextstuckDritteZeile').attr('class', 'creditsText');
 
-var erstesTextstuckZweiteZeileLange = document.getElementById('erstesTextstuckZweiteZeile').getComputedTextLength();
+var erstesTextstuckDritteZeileLange = document.getElementById('erstesTextstuckDritteZeile').getComputedTextLength();
 
 creditsCanvas.append("svg:a")
-    .attr("xlink:href", "https://docs.google.com/spreadsheets/d/1jLNfP3iuteUJrH0zS9qWONskyKh9pFcl1hKSlgEc-I8/edit#gid=1578718062")
+    .attr("xlink:href", "https://github.com/owid/chartbook")
     .attr("target", "_blank")
     .append("svg:text")
-    .attr("x", xorigin + erstesTextstuckZweiteZeileLange + 3)
+    .attr("x", xorigin + erstesTextstuckDritteZeileLange + 3)
     .attr("y", yorigin + 40)
     .attr('class', 'creditsLink')
-    .text("database");
+    .text("repository");
