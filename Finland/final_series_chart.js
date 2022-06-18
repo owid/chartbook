75ff6c91d71b33ea9390e9382e78b4ce238e824c
Cam Appel <cameron@ourworldindata.org>
2022-06-18 14:20:53 +0100

    finland

--- Finland/final_series_chart.js
+++ Finland/final_series_chart.js
@@ -751,10 +751,10 @@ creditsCanvas
 var erstesTextstuckDritteZeileLange = document.getElementById('erstesTextstuckDritteZeile').getComputedTextLength();
 
 creditsCanvas.append("svg:a")
-    .attr("xlink:href", "https://github.com/owid/chartbook")
+    .attr("xlink:href", "https://docs.google.com/spreadsheets/d/1jLNfP3iuteUJrH0zS9qWONskyKh9pFcl1hKSlgEc-I8/edit#gid=1578718062")
     .attr("target", "_blank")
     .append("svg:text")
     .attr("x", xorigin + erstesTextstuckDritteZeileLange + 3)
     .attr("y", yorigin + 40)
     .attr('class', 'creditsLink')
-    .text("repository");
+    .text("database");
